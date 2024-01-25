0d1c46f94e03f48efda06c48620c5c99a03c8b3a
Aleksandar Stevkovski <astevkovski87@gmail.com>
2024-01-25 23:14:35 +0100

    domasna 6

---
+++ nodeJS_domasna6/handlers/handler.js
@@ -0,0 +1,39 @@
+function handleHompage(req, res) {
+  res.writeHead(200, { "Content-type": "text/html" });
+  res.end("<h1>Welcome to our Homepage</h1>");
+}
+
+function handleEmail(req, res, email) {
+  const emailPattern = /^([a-z0-9.-]{3,})@([a-z]+)\.([a-z]{2,})/g;
+  if (emailPattern.test(email)) {
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.end(`<h2>Hello, Your email is ${email} </h2>`);
+  } else {
+    res.writeHead(404, { "Content-type": "text/html" });
+    res.end("<h2>Invalid Email</h2>");
+  }
+}
+
+function handleLocation(req, res, city, country) {
+  const cityPattern = /^([a-zA-Z]{2,})/;
+  const countryPattern = /^([a-zA-Z]{4,})/;
+  if (cityPattern.test(city) && countryPattern.test(country)) {
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.end(`<h2>Your city is ${city} and your country is ${country}</h2>`);
+  } else {
+    res.writeHead(404, { "Content-type": "text/html" });
+    res.end("<h2>Invalid City or Country</h2>");
+  }
+}
+
+function handlePageNotFound(req, res) {
+  res.writeHead(404, { "Content-type": "text/html" });
+  res.end("<h2> Oooops, Page not Found</h2>");
+}
+
+module.exports = {
+  handleHompage,
+  handleEmail,
+  handleLocation,
+  handlePageNotFound,
+};
---
+++ nodeJS_domasna6/index.js
@@ -0,0 +1,30 @@
+const http = require("http");
+const url = require("url");
+
+const {
+  handleHompage,
+  handleEmail,
+  handlePageNotFound,
+  handleLocation,
+} = require("./handlers/handler");
+
+const server = http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+
+  if (parsedUrl.pathname === "/" || parsedUrl.pathname === "/homepage") {
+    handleHompage(req, res);
+  } else if (parsedUrl.pathname === "/email") {
+    handleEmail(req, res, parsedUrl.query.email);
+    console.log(parsedUrl.query.email);
+  } else if (parsedUrl.pathname === "/location") {
+    handleLocation(req, res, parsedUrl.query.city, parsedUrl.query.country);
+  } else {
+    handlePageNotFound(req, res);
+  }
+});
+
+const port = 5000;
+
+server.listen(port, () => {
+  console.log("Server is listening on port 5000");
+});
